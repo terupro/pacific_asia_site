0fa4294c598ea8301301f46f68fc26e091f50762
‘terupro’ <terupro911@gmail.com>
2022-03-11 17:58:46 +0900

    first commit

---
+++ apt-pc/sidebar-common.php
@@ -0,0 +1,10 @@
+      <div class="rss_link">
+        <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/ico_rss.png" width="215" height="29" alt="こちらのRSSを購読する。" /></a>
+      </div>
+      <div class="textwidget">
+        <p>
+          <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/bnr_blog.jpg" class="" width="215" height="110" alt="社長ブログ" />ジャカルタで働く社長のブログ。</a>
+        </p>
+      </div>
+      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=215&height=342&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="215" height="342" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <a class="twitter-timeline" data-height="350" href="https://twitter.com/TwitterDev/lists/national-parks?ref_src=twsrc%5Etfw">A Twitter List by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
---
+++ apt-pc/sidebar.php
@@ -0,0 +1,4 @@
+<div id="sidebar">
+<?php get_template_part('sidebar-common'); ?>
+</div><!-- end #sidebar -->
+
